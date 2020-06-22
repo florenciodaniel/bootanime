45389bb5b2689d20940d3d0ec0c2c2e1c1dc7d9b
florenciodaniel <florenciodaniel69@gmail.com>
2020-06-22 13:06:30 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>BOOT ANIME</title>
+        <link href="bootanime.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="apresentacao">
+            <h1>Este projeto visa criar nossas proprias animações para paginas</h1>
+            <h1>Este projeto é recriado apartir de uma aula no youtube</h1>
+            <a href="https://youtu.be/pKTOT63X9XQ" rel="nofollow" target="_blank"><h1>LINK DO YOUTUBE</h1></a>
+        </div>
+        <div class="container">
+            <div class="data">
+                <p>data-boot_flip="esquerda"</p>
+            </div>
+            <div class="box" data-boot_flip="esquerda">
+                <img src="lolita_jft.jpg" alt=""/>
+            </div>
+        </div>
+        
+        <div class="container">
+            <div class="data">
+                <p>data-boot_flip="topo"</p>
+            </div>
+            <div class="box" data-boot_flip="topo">
+                <img src="lolita_jft.jpg" alt=""/>
+            </div>
+        </div>
+        
+        <div class="container">
+            <div class="data">
+                <p>data-boot_flip="baixo"</p>
+            </div>
+            <div class="box" data-boot_flip="baixo">
+                <img src="lolita_jft.jpg" alt=""/>
+            </div>
+        </div>
+        
+        <div class="container">
+            <div class="data">
+                <p>data-boot_flip="direita"</p>
+            </div>
+            <div class="box" data-boot_flip="direita">
+                <img src="lolita_jft.jpg" alt=""/>
+            </div>
+        </div>
+
+
+        <script src="bootanime.js" type="text/javascript"></script>
+    </body>
+</html>
